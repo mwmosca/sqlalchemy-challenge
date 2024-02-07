87e6797a1a2fdf03b05a05b88467dbd65b0ca062
mwmosca <mwmosca93@gmail.com>
2024-02-06 23:43:26 -0500

    modified:   app.py 	modified:   climate.ipynb

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -2,6 +2,7 @@
 import datetime as dt
 
 from flask import Flask, jsonify
+from numpy import ravel
 from pathlib import Path
 from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
@@ -22,8 +23,6 @@ Base.prepare(autoload_with=engine)
 Station = Base.classes.station
 Measurement = Base.classes.measurement
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -50,16 +49,56 @@ def welcome():
 
 @app.route('/api/v1.0/precipitation')
 def precipitation_analysis():
-    # Find the most recent date in the data set.
-    most_recent_date = dt.datetime.strptime(session.query(func.max(Measurement.date)).first()[0], '%Y-%m-%d').date()
-    
-    # Calculate the date one year from the last date in data set.
-    target_date = most_recent_date - dt.timedelta(days=365)
+    with Session(engine) as session:
+        # Find the most recent date in the data set.
+        date_query = session.query(func.max(Measurement.date)).first()
+        most_recent_date = dt.datetime.strptime(date_query[0], '%Y-%m-%d').date()
+        
+        # Calculate the date one year from the last date in data set.
+        target_date = most_recent_date - dt.timedelta(days=365)
 
-    # Perform a query to retrieve the data and precipitation scores
-    prcp_query = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date.between(target_date, most_recent_date))
-    
-    return 'Hi'
+        # Perform a query to retrieve the data and precipitation scores
+        prcp_query = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date.between(target_date, most_recent_date)).all()
+
+    # Display the query result as a jsonified dictionary
+    precipitation_analysis_results = dict(prcp_query)
+    return jsonify(precipitation_analysis_results)
+
+
+@app.route('/api/v1.0/stations')
+def get_stations():
+    with Session(engine) as session:
+        # Perform a query to retrieve the station names
+        station_query = session.query(Station.name).all()
+
+    # Flatten the query result into a normal list to jsonify and display
+    station_list = list(ravel(station_query))
+    return jsonify(station_list)
+
+
+@app.route('/api/v1.0/tobs')
+def temperature_analysis():
+    with Session(engine) as session:
+        # Find the most recent date in the data set.
+        date_query = session.query(func.max(Measurement.date)).first()
+        most_recent_date = dt.datetime.strptime(date_query[0], '%Y-%m-%d').date()
+
+        # Calculate the date one year from the last date in data set.
+        target_date = most_recent_date - dt.timedelta(days=365)
+
+        # Design a query to find the most active station
+        station_count = func.count(Measurement.station)
+        station_activity_query = session.query(Measurement.station, station_count).group_by(Measurement.station).order_by(station_count.desc()).all()
+        most_active_station = station_activity_query[0].station
+
+        # Using the most active station id query the last 12 months of temperature observation data for this station
+        tobs_query = session.query(Measurement.date, Measurement.tobs).\
+            filter(Measurement.station == most_active_station, Measurement.date.between(target_date, most_recent_date)).all()
+
+    # Display the query result as a jsonified dictionary
+    temperature_analysis_results = dict(tobs_query)
+    return jsonify(temperature_analysis_results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
